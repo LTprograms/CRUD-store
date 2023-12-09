90564383b7b3d22c4415731861c6c7bb2f6aec98
Luis LT <105554066+LTprograms@users.noreply.github.com>
2023-12-09 16:53:43 -0500

    api fixed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ClientController::class)->group(function () {
+    Route::get('/clients','index');
+    Route::get('/client/{id}','show');
+    Route::post('/client','store');
+    Route::put('/client/{id}','update');
+    Route::delete('/client/{id}','destroy');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products','index');
+    Route::get('/product/{id}','show');
+    Route::post('/product','store');
+    Route::put('/product/{id}','update');
+    Route::delete('/product/{id}','destroy');
+});
+
+Route::controller(PurchaseController::class)->group(function () {
+    Route::get('/purchases','index');
+    Route::get('/purchase/{id}','show');
+    Route::post('/purchase','store');
+    Route::put('/purchase/{id}','update');
+    Route::delete('/purchase/{id}','destroy');
 });
